68caf4b4106651f181c166c3fb42fc40b5e2f097
Sima Nerush <2002ssn@gmail.com>
2022-01-22 22:23:26 -0800

    Initial commit

---
+++ EssenceToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EssenceToDo
+//
+//  Created by Serafima Nerush on 1/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EssenceToDo/EssenceToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssenceToDoApp.swift
+//  EssenceToDo
+//
+//  Created by Serafima Nerush on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct EssenceToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
